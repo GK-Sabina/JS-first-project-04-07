11673ebfe581129370434c632d8ea782e816d525
Sabina <justkrupasabina@gmail.com>
2023-07-04 11:08:15 +0300

    Example data type

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log(undefined);
+console.log(undefined);
+
+// boolean
+console.log(true);
+console.log(false);
+
+// string
+console.log('true');
+console.log("true");
+console.log("I'm a teacher")
+
+// numeric
+console.log(12345678);
+console.log(123.45678);
+console.log(-Infinity);
+console.log(NaN);
+
+// bigint
+console.log(1n);
