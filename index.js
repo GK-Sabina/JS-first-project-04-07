69e6cc309384b2968599e58b7768542e86f6416d
Sabina <justkrupasabina@gmail.com>
2023-07-05 08:52:10 +0300

    Add const-homework

--- index.js
+++ index.js
@@ -3,26 +3,30 @@
 // регистрозависимые
 // отвечает на вопрос что в ней
 
+// console.log(userName, userSname);
+// {
+//     let userName="Sabina";
+//     console.log("in block-", userName)
+// }
+
+// let userName; 
+// console.log(userName);
+// userName = "Brad";
+// console.log(userName);
 
 
-
+// const userSname = "Pitt";
 
 // console.log(userName, userSname);
-{
-    let userName="Sabina";
-    console.log("in block-", userName)
-}
 
-let userName; 
-console.log(userName);
-userName = "Brad";
-console.log(userName);
+// const userGender = "male";
 
+// console.log(userName, userSname, userGender);
 
-const userSname = "Pitt";
+const userAge = 23; -Number
+const isAdult = true; -Boolean
+const programmingLanguage = "C++"; -String
+const typePets = null; -null
+const counterDaysWeek = 7; -Number
 
-console.log(userName, userSname);
-
-const userGender = "male";
-
-console.log(userName, userSname, userGender);
+console.log(123)
