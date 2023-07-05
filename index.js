6a5c33edfb1482b8a688c8d761f7c269b6d2e8d9
Sabina <justkrupasabina@gmail.com>
2023-07-05 10:42:29 +0300

    Add '+' '-' '*' '/'

--- index.js
+++ index.js
@@ -23,10 +23,28 @@
 
 // console.log(userName, userSname, userGender);
 
-const userAge = 23; -Number
-const isAdult = true; -Boolean
-const programmingLanguage = "C++"; -String
-const typePets = null; -null
-const counterDaysWeek = 7; -Number
+// const userAge = 23; -Number
+// const isAdult = true; -Boolean
+// const programmingLanguage = "C++"; -String
+// const typePets = null; -null
+// const counterDaysWeek = 7; -Number
 
-console.log(123)
+const AMOUNT_DAYS_OF_WEEK = 7;
+
+let programLang = 'C++';
+
+let sens = "I'm a \nteacher";
+console.log(sens);
+
+let value1 = "12";
+let value2 = null;
+
+let result1 = value1 + value2;
+let result2 = value1 - value2;
+let result3 = value1 * value2;
+let result4 = value1 / value2;
+
+console.log('+', result1, typeof result1);
+console.log('-', result2, typeof result2);
+console.log('*', result3, typeof result3);
+console.log('/', result4, typeof result4);
